13cd45556720a5273d84c6039ff894ebac63f6c3
AzKir <a.kirykhina@yandex.ru>
2023-01-18 16:37:26 +0300

    feat: it's alive
    Form is more interactive now.

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,12 @@ let popup = document.getElementById('editProfile')
 let profileEditButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = document.querySelector('.popup__close-button');
 
+// форма
+let formElement = document.querySelector('.form');
+let nameInput = document.querySelector('.form__info_type_name');
+let introInput = document.querySelector('.form__info_type_intro');
+let profileName = document.querySelector('.profile__name');
+let profileIntro = document.querySelector('.profile__intro');
 
 // базовые функции
 function openPopup() {
@@ -15,3 +21,15 @@ function closePopup() {
 // обработчик событий
 profileEditButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
+
+
+// отправка формы (редактирование профиля)
+function handleProfileFormSubmit (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileIntro.textContent = introInput.value;
+    closePopup();
+}
+
+// обработчик к форме (редактирования профиля)
+formElement.addEventListener('submit', handleProfileFormSubmit);
