991ce9ab8cb8499909fd696c1b99438430c9e205
AzKir <a.kirykhina@yandex.ru>
2023-03-29 20:11:48 +0300

    fix
    names

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,8 +3,8 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
     constructor(popupSelector) {
         super(popupSelector);
-        this._image = this._popup.querySelector('.popup__image');
-        this._subtitle = this._popup.querySelector('.popup__subtitle');
+        this._image = this._popupElement.querySelector('.popup__image');
+        this._subtitle = this._popupElement.querySelector('.popup__subtitle');
     };
 
     open(name, link) {
