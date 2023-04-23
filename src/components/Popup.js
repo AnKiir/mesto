c21b8e0248a5283fe2c4fb65ad267388badc400c
AzKir <a.kirykhina@yandex.ru>
2023-04-23 23:16:38 +0300

    fix
    closes in any way desired

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,7 +1,7 @@
 export default class Popup {
     constructor(popupSelector) {
         this._popup = document.querySelector(popupSelector.popupSelector);
-       // this._closeButton = this._popup.querySelector('.popup__button_type_close');
+        this._closeButton = this._popup.querySelector('.popup__button_type_close');
         this._handleEscClose = this._handleEscClose.bind(this);
     };
 
